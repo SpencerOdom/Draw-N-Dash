a871b008c2b12537457cdc03af9d6fc9f576c87f
BV344 <123995573+BV344@users.noreply.github.com>
2024-10-30 17:37:15 -0700

    Adding Pencil & Eraser functionality
    Pencil Button: Has icon and works
    Eraser Button: Has Icon and works

--- Gamemodes/Normal/canvas.gd
+++ Gamemodes/Normal/canvas.gd
@@ -1,13 +1,57 @@
+## This is canvas.gd
+#extends Node2D
+#
+#
+#@onready var _lines: Node2D = $Line2D
+#
+#var _pressed: bool = false
+#var _current_line: Line2D = null
+#
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton:
+		#if event.button_index == MOUSE_BUTTON_LEFT:
+			#_pressed = event.pressed
+			#
+			#if _pressed:
+				#_current_line = Line2D.new()
+				#_current_line.default_color = Color.BLACK
+				#_current_line.width = 12
+				#_lines.add_child(_current_line)
+				#_current_line.add_point(event.position)
+				#
+	#elif event is InputEventMouseMotion and _pressed:
+		#_current_line.add_point(event.position)
+
+# This is canvas.gd
 extends Node2D
 
-# TODO: Make it so that the player can only draw whats on the canvas.
-
-
 @onready var _lines: Node2D = $Line2D
+@onready var pencil_button = $PencilButton  # Assuming button nodes in the scene
+@onready var eraser_button = $EraserButton
 
 var _pressed: bool = false
 var _current_line: Line2D = null
+var tool_mode: String = "pencil"  # Tracks the current tool, either "pencil" or "eraser"
 
+# Load custom cursor images
+var pencil_cursor_texture = preload("res://Cursor_Images/pencil-cursor.png")
+var eraser_cursor_texture = preload("res://Cursor_Images/eraser-cursor.png")
+
+# Define hotspot positions for custom cursors
+var pencil_cursor_hotspot = Vector2(0, 16)  # Adjust as needed for the active part of the pencil
+var eraser_cursor_hotspot = Vector2(0, 16)  # Adjust as needed for the eraser center
+
+# Called when the pencil button is pressed
+func _on_pencil_button_pressed() -> void:
+	tool_mode = "pencil"
+	Input.set_custom_mouse_cursor(pencil_cursor_texture, Input.CURSOR_ARROW, pencil_cursor_hotspot)  # Set cursor to pencil image
+
+# Called when the eraser button is pressed
+func _on_eraser_button_pressed() -> void:
+	tool_mode = "eraser"
+	Input.set_custom_mouse_cursor(eraser_cursor_texture, Input.CURSOR_ARROW, eraser_cursor_hotspot)  # Set cursor to eraser image
+
+# Main input function
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
@@ -15,10 +59,22 @@ func _input(event: InputEvent) -> void:
 			
 			if _pressed:
 				_current_line = Line2D.new()
-				_current_line.default_color = Color.BLACK
-				_current_line.width = 12
+				
+				if tool_mode == "pencil":
+					_current_line.default_color = Color.BLACK
+					_current_line.width = 12
+				elif tool_mode == "eraser":
+					_current_line.default_color = Color.WHITE  # Transparent color for erasing
+					_current_line.width = 24  # Larger width for the eraser effect
+
 				_lines.add_child(_current_line)
 				_current_line.add_point(event.position)
 				
 	elif event is InputEventMouseMotion and _pressed:
 		_current_line.add_point(event.position)
+
+
+# Connect button signals in _ready
+func _ready():
+	pencil_button.connect("pressed", Callable(self, "_on_pencil_button_pressed"))
+	eraser_button.connect("pressed", Callable(self, "_on_eraser_button_pressed"))
---
+++ Library/QuickTextureButton.gd
@@ -0,0 +1,99 @@
+@tool
+extends TextureButton
+class_name QuickTextureButton
+
+
+@export var alpha_border: int = 16:
+	set(val):
+		alpha_border = val
+		set_textures()
+
+@export var icon: CompressedTexture2D:
+	set(val):
+		icon = val
+		set_textures()
+
+
+var see_through = Color(1.0, 1.0, 1.0, 0.0)
+
+
+func set_textures():
+	if not icon:
+		return
+	if alpha_border < 0:
+		return
+	
+	var __icon: ImageTexture = ImageTexture.create_from_image(icon.get_image())
+	
+	# button texture_normal (larger version of regular icon, but with alpha border)
+	var raw_icon_image: Image = __icon.get_image()
+	var raw_icon_size: Vector2i = raw_icon_image.get_size()
+	var icon_normal: Image = Image.create(
+		raw_icon_size.x + int(alpha_border)*2,
+		raw_icon_size.y + int(alpha_border)*2,
+		false,
+		Image.FORMAT_RGBA8,
+	)
+	for x in icon_normal.get_size().x:
+		for y in icon_normal.get_size().y:
+			# if we're out of the bounds of the smaller raw icon, then set a see-through pixel
+			# (as we're surrounding the original 128x128 icon with a 16px see-through
+			# border on all sides, making the new icon 160x160)
+			var is_on_border_x = x < alpha_border or x >= icon_normal.get_size().x - alpha_border
+			var is_on_border_y = y < alpha_border or y >= icon_normal.get_size().y - alpha_border
+			if is_on_border_x or is_on_border_y:
+				icon_normal.set_pixel(x, y, see_through)
+			else:
+				# otherwise, copy the smaller icon into the middle of the new larger icon
+				var pixel: Color = raw_icon_image.get_pixel(x-int(alpha_border), y-int(alpha_border))
+				icon_normal.set_pixel(x, y, pixel)
+	
+	texture_normal = ImageTexture.create_from_image(icon_normal)
+	
+	# button texture_hover: inverted icon
+	var icon_hover: Image = icon_normal.duplicate()
+	for x in icon_hover.get_size().x:
+		for y in icon_hover.get_size().y:
+			var pixel: Color = icon_hover.get_pixel(x, y)
+			icon_hover.set_pixel(x, y, pixel.inverted())
+	texture_hover = ImageTexture.create_from_image(icon_hover)
+	
+	# button texture_pressed: lighter inverted icon
+	var icon_pressed: Image = icon_hover.duplicate()
+	for x in icon_pressed.get_size().x:
+		for y in icon_pressed.get_size().y:
+			var pixel: Color = icon_pressed.get_pixel(x, y)
+			icon_pressed.set_pixel(x, y, pixel.lightened(0.5))
+	texture_pressed = ImageTexture.create_from_image(icon_pressed)
+	
+	# button texture_focused: just border, clear alpha in middle
+	var icon_focused: Image = icon_normal.duplicate()
+	for x in icon_focused.get_size().x:
+		for y in icon_focused.get_size().y:
+			# the alpha border on textuer_normal is 'alpha_border' wide, but the
+			# white rectangle is half that
+			var is_on_border_x = x < alpha_border/2.0 or x >= icon_focused.get_size().x - alpha_border/2.0
+			var is_on_border_y = y < alpha_border/2.0 or y >= icon_focused.get_size().y - alpha_border/2.0
+			if is_on_border_x or is_on_border_y:
+				icon_focused.set_pixel(x, y, Color.WHITE)
+			else:
+				# otherwise, make it see-through
+				icon_focused.set_pixel(x, y, see_through)
+	texture_focused = ImageTexture.create_from_image(icon_focused)
+	
+	# button texture_disabled: gray icon
+	var _texture_disabled: Texture = ImageTexture.create_from_image(icon_normal)
+	texture_disabled = to_grayscale(_texture_disabled)
+	
+#	var icon_disabled: Image = _texture_disabled.get_image()
+#	print("(QuickTextureButton) icon=%s, normal=%s, hover=%s, pressed=%s, focused=%s, disabled=%s" % [
+#		icon.get_size(), icon_normal.get_size(), icon_hover.get_size(),
+#		icon_pressed.get_size(), icon_focused.get_size(), icon_disabled.get_size(),
+#	])
+
+
+func to_grayscale(texture: ImageTexture) -> Texture:
+	var image = texture.get_image()
+	image.convert(Image.FORMAT_LA8)
+	image.convert(Image.FORMAT_RGBA8) # Not strictly necessary
+	return ImageTexture.create_from_image(image)
