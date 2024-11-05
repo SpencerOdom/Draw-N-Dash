462e778af5f7e2a6c46c6f9ff6191b38c1df66af
XX9rZbaT <cottrell@sonoma.edu>
2024-11-05 11:47:56 -0800

    Changed the layout of some stuff, for the future.

--- world.gd
+++ world.gd
@@ -1,29 +1,49 @@
 extends Node
 
+# TODO: The lobby should be loaded automatically.
+# TODO: The lobby will connect to the multiplayer on the games start.
+
+var multiplayer_peer = ENetMultiplayerPeer.new()
+
+# TODO: Put these in the ignore file.
+#const ADDRESS = ""
+#const PORT: int = 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#change_phase(0)
+	$".".add_child(load("res://Lobby/lobby_menu.tscn").instantiate())
+	
+	#if multiplayer_peer.get_connection_status() == 0:
+	#	print("Creating a connection.\n", get_stack())
+	#	multiplayer_peer.create_client(ADDRESS, PORT)
+	#	multiplayer.multiplayer_peer = multiplayer_peer
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	pass
 
+#@rpc
+#func client_func(data: int):
+#	print(data, '\n', get_stack())
 
-func _on_host_button_pressed() -> void:
-	#var current_scene_file = get_tree().current_scene.select_file_path
-	var gamemodedropdown = $CanvasLayer/MainMenu/VBoxContainer/VBoxContainer/HostGameField/GamemodeDropDown
-	print("Gamemode index ", gamemodedropdown.selected, " selected")
+@rpc("any_peer")
+func change_phase(data: int):
+	print("next phase, ", data, '\n', get_stack())
+	var level = $"."
 	
-	match gamemodedropdown.selected:
+	# Clear current level.
+	for c in level.get_children():
+		level.remove_child(c)
+		c.queue_free()
+	
+	#Adding new level.
+	match data:
 		0:
-			print("Starting gamemode Normal.")
-			get_tree().change_scene_to_file("res://Gamemodes/Normal/starting_word_prompt.tscn")
-			return # Will this cause issues?
+			level.add_child(load("res://Lobby/lobby_menu.tscn").instantiate())
+		# Normal Gamemode
 		1:
-			# More game modes will follow.
-			print("Case 1")
-	print_stack()
-	
-	pass # Replace with function body.
+			level.add_child(load("res://Gamemodes/Normal/starting_word_prompt.tscn").instantiate())
+	pass
