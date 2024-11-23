301a8978470b4eca9eb7a52bbfa9b0791edd1e3f
XX9rZbaT <cottrell@sonoma.edu>
2024-11-23 11:46:22 -0800

    Fixed the RPC functions in the lobby.

--- multiplayer_manager.gd
+++ multiplayer_manager.gd
@@ -2,9 +2,10 @@ extends Node
 
 @onready var multiplayer_peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
+signal intstring_1(_i: int, _s: String)
 
-#const ADDRESS = "130.157.167.146"
-const ADDRESS = "130.157.167.94"
+const ADDRESS = "130.157.167.146"
+#const ADDRESS = "130.157.167.94"
 const PORT: int = 80
 #const ADDRESS = "130.157.170.101"
 #const PORT: int = 8101
@@ -47,3 +48,88 @@ func _on_scene_changed():
 func client_connected(_id: int):
 	print(_id, " connected.\n", get_stack())
 	pass
+
+
+
+
+
+# ************************************
+# Main Menu
+# ************************************
+
+
+func to_lobby() -> void:
+	# TODO: Connection should go here.
+	get_tree().change_scene_to_file("res://Lobby/lobby_menu.tscn")
+	pass
+
+
+
+# ************************************
+# Lobby
+# ************************************
+
+
+func get_player_list() -> void:
+	rpc_id(1, "send_player_list", multiplayer_peer.get_unique_id())
+	pass
+
+
+func call_server_to_start_game() -> void:
+	rpc_id(1, "server_start_game")
+	pass
+
+
+@rpc("any_peer")
+func append_player_to_lobby_list(_id: int, _user: String) -> void:
+	emit_signal('intstring_1', _id, _user)
+	pass
+
+
+@rpc("any_peer")
+func client_start_game() -> void:
+	print("Starting Game.\n", get_stack())
+	get_tree().change_scene_to_file("res://Gamemodes/Normal/starting_word_prompt.tscn")
+	pass
+
+# Server
+
+@rpc("any_peer")
+func send_player_list(_id: int) -> void:
+	pass
+
+@rpc("any_peer")
+func server_start_game() -> void:
+	pass
+
+
+
+
+# ************************************
+# Starting Guessing phase
+# ************************************
+
+
+
+
+
+# submit_prompt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# hi
