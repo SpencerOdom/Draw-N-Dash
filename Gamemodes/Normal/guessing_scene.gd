253a15431cfe644a67998330ac0d7554c9ca511f
XX9rZbaT <cottrell@sonoma.edu>
2024-11-23 21:23:19 -0800

    Finished the serialization and deserialization HELL YES

--- Gamemodes/Normal/guessing_scene.gd
+++ Gamemodes/Normal/guessing_scene.gd
@@ -23,10 +23,10 @@ func _ready():
 	#$Timer.start()
 	# Disconnect the signal if it's already connected
 	if !$Timer.is_connected("timeout", Callable(self, "_on_timer_timeout")):
-		#$Timer.disconnect("timeout", Callable(self, "_on_timer_timeout"))
-	
 		# Reconnect the signal
 		$Timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+		
+	if !button.is_connected("pressed", Callable(self, "_on_quick_texture_button_pressed")):
 		button.connect("pressed", Callable(self, "_on_quick_texture_button_pressed"))
 		
 	$Timer.wait_time = MultiplayerManager.prompt_time
@@ -62,6 +62,44 @@ func guessing_scene_send_prompt() -> void:
 	pass
 
 
-func guessing_scene_set_drawing(_img: Node2D) -> void:
-	add_child(_img)
+
+func deserialize_image(serialized_data: Dictionary) -> Line2D:
+	var node = Line2D.new()
+	
+	node.name = serialized_data["name"]
+	node.antialiased = serialized_data["antialiased"]
+	node.begin_cap_mode = serialized_data["begin_cap_mode"]
+	node.closed = serialized_data["closed"]
+	node.default_color = serialized_data["default_color"]
+	node.end_cap_mode = serialized_data["end_cap_mode"]
+	node.gradient = serialized_data["gradient"]
+	node.joint_mode = serialized_data["joint_mode"]
+	node.points = serialized_data["points"]
+	node.round_precision = serialized_data["round_precision"]
+	node.sharp_limit = serialized_data["sharp_limit"]
+	node.sharp_limit = serialized_data["sharp_limit"]
+	node.texture = serialized_data["texture"]
+	node.width = serialized_data["width"]
+	node.width_curve = serialized_data["width_curve"]
+	
+	# Recursively add children
+	for child_data in serialized_data["children"]:
+		var child_node = deserialize_image(child_data) # Recursively recreate child nodes
+		node.add_child(child_node)
+
+	return node
+
+
+
+func guessing_scene_set_drawing(_img: Dictionary) -> void:
+	#add_child(_img)
+	#add_child(_img.instantiate())
+	
+	print("guessing scene has recieve the \"image\": ", _img, '\n\n')
+	
+	# TODO: Deserialize the data
+	var deserialized_img:Line2D = deserialize_image(_img)
+	
+	$Node2D.add_child(deserialized_img)
+	
 	pass
