63360d5198a805f542125010b500b5da0d121fdb
XX9rZbaT <cottrell@sonoma.edu>
2024-11-24 19:12:48 -0800

    Changes to end phase and multiplayer.

--- Final_scenes_gadot/finalscenes/final_scene.gd
+++ Final_scenes_gadot/finalscenes/final_scene.gd
@@ -5,43 +5,147 @@ var display_image_path: String = "res://Mainmenuimage/menuupdate.jpg"
 var desired_size: Vector2 = Vector2(500, 500)  # Desired width and height in pixels
 var label_text: String = "Image has been updated!"  # The text to display in the Label
 
-var switch = true
-
 
 func _ready():
 	# Connect the button's pressed signal
-	var button = get_node("QuickTextureButton")
-	button.connect("pressed", Callable(self, "_on_quick_texture_button_pressed"))
-	print("Debug: Button pressed!")
+	if !$QuickTextureButton.is_connected("pressed", Callable(self, "_on_quick_texture_button_pressed")):
+		print("Final Scene is having to connect button.\n", get_stack())
+		$QuickTextureButton.connect("pressed", Callable(self, "_on_quick_texture_button_pressed"))
+
 
 
 func _on_quick_texture_button_pressed() -> void:
-	
-	if(switch):
-		MultiplayerManager.get_next_prompt()
-		switch = false
-	else:	
-		MultiplayerManager.get_next_drawing()
-		switch = true
-		
+	MultiplayerManager.get_next_submition()
+
 
 
 func _on_tree_entered() -> void:
+	MultiplayerManager.shot_cycle_signal.connect(start_cycle)
 	MultiplayerManager.shot_prompt_signal.connect(display_prompt)
 	MultiplayerManager.shot_drawing_signal.connect(display_drawing)
-	
-func display_prompt(txt: String) -> void:
-	var parent_node = $ScrollContainer/VBoxContainer
-	# Create a new Label node
-	var label = Label.new()
-	label.text = txt
-	parent_node.add_child(label)
-	
 
-func display_drawing(img: Node2D) -> void:
-	var parent_node = $ScrollContainer/VBoxContainer
-	parent_node.add_child(img)  # Add the image (now referencing img) to the scene
-	
 func _on_tree_exiting() -> void:
+	MultiplayerManager.shot_cycle_signal.disconnect(start_cycle)
 	MultiplayerManager.shot_prompt_signal.disconnect(display_prompt)
 	MultiplayerManager.shot_drawing_signal.disconnect(display_drawing)
+
+
+
+func start_cycle(id:int, username:String, txt: String) -> void:
+	for child in $ScrollContainer/VBoxContainer.get_children():
+		$ScrollContainer/VBoxContainer.remove_child(child)
+		child.queue_free()  # Properly deletes the child node
+	display_prompt(id, username, txt)
+	pass
+
+
+
+func display_prompt(id:int, username:String, txt: String) -> void:
+	print("display_prompt\n", get_stack())
+	
+	var prompt_container = HBoxContainer.new()
+	var author_credit_container = VBoxContainer.new()
+	
+	var icon = TextureRect.new()
+	icon.texture = load("res://icon.svg")
+	author_credit_container.add_child(icon)
+	
+	var name_containter = Label.new()
+	name_containter.text = username
+	author_credit_container.add_child(name_containter)
+	
+	var id_containter = Label.new()
+	id_containter.text = str(id)
+	author_credit_container.add_child(id_containter)
+	
+	prompt_container.add_child(author_credit_container)
+	
+	var label = Label.new()
+	label.add_theme_font_size_override("font_size", 35)
+	label.text = txt
+	prompt_container.add_child(label)
+	
+	
+	
+	$ScrollContainer/VBoxContainer.add_child(prompt_container)
+	pass
+
+
+
+
+
+
+
+
+func deserialize_image(serialized_data: Dictionary) -> Line2D:
+	var node = Line2D.new()
+	
+	node.name = serialized_data["name"]
+	node.antialiased = serialized_data["antialiased"]
+	node.begin_cap_mode = serialized_data["begin_cap_mode"]
+	node.closed = serialized_data["closed"]
+	node.default_color = serialized_data["default_color"]
+	node.end_cap_mode = serialized_data["end_cap_mode"]
+	node.gradient = serialized_data["gradient"]
+	node.joint_mode = serialized_data["joint_mode"]
+	node.points = serialized_data["points"]
+	node.round_precision = serialized_data["round_precision"]
+	node.sharp_limit = serialized_data["sharp_limit"]
+	node.sharp_limit = serialized_data["sharp_limit"]
+	node.texture = serialized_data["texture"]
+	node.width = serialized_data["width"]
+	node.width_curve = serialized_data["width_curve"]
+	
+	# Recursively add children
+	for child_data in serialized_data["children"]:
+		var child_node = deserialize_image(child_data) # Recursively recreate child nodes
+		node.add_child(child_node)
+
+	return node
+
+
+
+
+
+
+func display_drawing(id:int, username:String, img: Dictionary) -> void:
+	
+	print("display_drawing\n", get_stack())
+	
+	
+	var drawing_container = HBoxContainer.new()
+	
+	var image = Node2D.new()
+	image.add_child(deserialize_image(img))
+	drawing_container.add_child(image)
+	
+	var author_credit_container = VBoxContainer.new()
+	
+	var icon = TextureRect.new()
+	icon.texture = load("res://icon.svg")
+	author_credit_container.add_child(icon)
+	
+	var name_containter = Label.new()
+	name_containter.text = username
+	author_credit_container.add_child(name_containter)
+	
+	var id_containter = Label.new()
+	id_containter.text = str(id)
+	author_credit_container.add_child(id_containter)
+	
+	drawing_container.add_child(author_credit_container)
+	
+	
+	
+	$ScrollContainer/VBoxContainer.add_child(drawing_container)  # Add the image (now referencing img) to the scene
+
+
+
+
+
+
+
+
+
+
+#
